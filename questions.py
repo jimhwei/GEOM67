bea0c236e9a458bac21bceb610bdc5001405e3f7
wemjii <60511633+wemjii@users.noreply.github.com>
2020-11-30 17:17:38 -0500

    questions

--- questions.py
+++ questions.py
@@ -3,7 +3,34 @@ import csv
 # f = open("SnakesQs.csv", "a")
 
 #open and read the file after the appending:
-f = open("SnakesQs.csv", "r")
-print(f.read())
+# f = open("SnakesQs.csv", "r")
 
-f.close()
+with open('SnakesQs.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    question = []
+    option1 = []
+    option2 = []
+    option3 = []
+    option4 = []
+    answers = []
+
+    for row in readCSV:
+        questions = row[0]
+        A = row[1]
+        B = row[2]
+        C = row[3]
+        D = row[4]
+        answer = [5]
+
+        question.append(questions)
+        option1.append(A)
+        option2.append(B)
+        option3.append(C)
+        option4.append(D)
+        answers.append(answer)
+
+    print(question)
+    print(option1)
+    print(option2)
+    print(option3)
+    print(option4)
