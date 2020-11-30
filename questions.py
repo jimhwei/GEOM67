d99f052a795f71c655f235465267ff893ce1d287
wemjii <60511633+wemjii@users.noreply.github.com>
2020-11-30 16:23:39 -0500

    updates csv

--- questions.py
+++ questions.py
@@ -1,10 +1,9 @@
 import csv
 # tell it where the cvs file is....
-f = open("SnakesQs.csv", "a")
-f.close()
-
+# f = open("SnakesQs.csv", "a")
 
 #open and read the file after the appending:
 f = open("SnakesQs.csv", "r")
 print(f.read())
 
+f.close()
