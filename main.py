9d55efdbe8a53c64ea47b199d451d0c47a29a2fe
Jeonghyeok Seo <jeonghyeok.seo@flemingcollege.ca>
2020-11-30 15:23:05 -0500

    test from jeonghyeok

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ if game1 == "True":
     snake.forward(180)
 
 #kedie tryna commit*
+#Test 
 # # how should the user interact with this screen
 # # it should be intermediate inputs
 wn.exitonclick()
