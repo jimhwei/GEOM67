9e99106a61cc159028dc4abf74d0c509911ccee0
Your Name <kedie.perdue@flemingcollege.ca>
2020-11-30 16:21:37 -0500

    read cvs

--- questions.py
+++ questions.py
@@ -1,9 +1,10 @@
 import csv
 # tell it where the cvs file is....
-f = open("C:\GEOM67\snakes\PSPSnakesLadders\SnakesQs.csv", "a")
+f = open("SnakesQs.csv", "a")
 f.close()
 
+
 #open and read the file after the appending:
-f = open("C:\GEOM67\snakes\PSPSnakesLadders\SnakesQs.csv", "r")
+f = open("SnakesQs.csv", "r")
 print(f.read())
 
