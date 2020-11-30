b9e40b35e2a47fb2be2536fb9d63195245ace31a
wemjii <60511633+wemjii@users.noreply.github.com>
2020-11-30 14:39:56 -0500

    initial

---
+++ main.py
@@ -0,0 +1,15 @@
+# Awesome Snakes & Turtles Game
+# Created By: IPGRP2 - J. Debono, K. Perdue, J. Seo, H. Wei
+# November 2020
+
+# Turtle Library Reference
+# https://realpython.com/beginners-guide-python-turtle/
+
+import turtle
+import csv
+
+# setting up the turtle
+wn = turtle.Screen()
+wn.bgcolor("grey")
+snake = turtle.Turtle()
+snake.shape("turtle")
