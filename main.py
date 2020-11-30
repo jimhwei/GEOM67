98e31dfafd411c176ccd27c749a3a3da2ae92f9f
wemjii <60511633+wemjii@users.noreply.github.com>
2020-11-30 15:31:49 -0500

    change variable snake to user

--- main.py
+++ main.py
@@ -15,17 +15,22 @@ wn = turtle.Screen()
 wn.bgcolor("grey")
 # turtle.getshapes()
 
-snake = turtle.Turtle()
-snake.penup()
-snake.goto(-350,350)
-snake.shape("turtle")
-snake.color("green")
+# initiate turtle
+user = turtle.Turtle()
+user.penup()
+user.goto(-350,350)
+user.shape("turtle")
+user.color("green")
 
-# game 1 input and output
+# coin flip game
+
+# create a die
+die = [1,2,3,4,5,6]
+
 game1 = input("True")
 if game1 == "True":
-    snake.forward(180)
+    user.forward(180)
 
-# # how should the user interact with this screen
-# # it should be intermediate inputs
+# how should the user interact with this screen
+# it should be intermediate inputs
 wn.exitonclick()
