0764a9ef9afc358cfd6d53649600badc37758e8a
wemjii <60511633+wemjii@users.noreply.github.com>
2020-11-30 14:44:52 -0500

    turtles

--- main.py
+++ main.py
@@ -8,8 +8,15 @@
 import turtle
 import csv
 
+user = input("Select turtle object/color: ")
+
 # setting up the turtle
 wn = turtle.Screen()
 wn.bgcolor("grey")
 snake = turtle.Turtle()
 snake.shape("turtle")
+snake.color("green")
+snake.pensize(50)
+
+# how should the user interact with this screen
+wn.exitonclick()
