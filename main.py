6b9f30fb836df85dd66ae644d0db99b570299d04
Jeonghyeok Seo <jeonghyeok.seo@flemingcollege.ca>
2020-11-30 16:04:47 -0500

    background grid

--- main.py
+++ main.py
@@ -7,16 +7,7 @@
 
 import turtle
 import csv
-<<<<<<< HEAD
-#Background image
-screen = turtle.Screen()
-screen.setup(900,600)
-screen.bgpic('boardgamegrid-01.png')
-screen.update()
-
-=======
 import random
->>>>>>> 6e1934f63d3171a0a0e9ef290f96382a33bf7721
 
 #Background image
 screen = turtle.Screen()
@@ -33,23 +24,6 @@ wn = turtle.Screen()
 # user gets to choose shapes
 # turtle.getshapes()
 
-<<<<<<< HEAD
-snake = turtle.Turtle()
-snake.penup()
-snake.goto(-300,120)
-snake.shape("turtle")
-snake.color("green")
-
-# game 1 input and output
-game1 = input("True")
-if game1 == "True":
-    snake.forward(180)
-
-#kedie tryna commit*
-#Test 
-# # how should the user interact with this screen
-# # it should be intermediate inputs
-=======
 # initiate turtle
 user = turtle.Turtle()
 user.penup()
@@ -67,9 +41,4 @@ user.color("green")
 
 # how should the user interact with this screen
 # it should be intermediate inputs
-<<<<<<< HEAD
->>>>>>> 6e1934f63d3171a0a0e9ef290f96382a33bf7721
 wn.exitonclick()
-=======
-wn.exitonclick()
->>>>>>> 183e80ea2f9aff17a201441d056be362ce156967
