3c50f64844e1fae0fa0de1635897f7123a0abddc
Jeonghyeok Seo <jeonghyeok.seo@flemingcollege.ca>
2020-12-02 14:56:35 -0500

    Turtle moving

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ wn = turtle.Screen()
 # initiate turtle
 user = turtle.Turtle()
 user.penup()
-user.goto(-300,120)
+user.goto(-180,120)
 user.shape("turtle")
 user.color("green")
 
@@ -41,4 +41,47 @@ user.color("green")
 
 # how should the user interact with this screen
 # it should be intermediate inputs
+
+
+user.forward(275)
+user.shapesize(2,2,1)
+
+
+user.forward(275)
+user.shapesize(3,3,1)
+
+
+user.right(90)
+user.forward(170)
+user.right(90)
+user.shapesize(4,4,1)
+
+user.right(2)
+user.forward(275)
+user.left(2)
+user.shapesize(5,5,1)
+
+user.right(2)
+user.forward(290)
+user.left(2)
+user.shapesize(6,6,1)
+
+user.left(70)
+user.forward(190)
+user.left(110)
+user.shapesize(7,7,1)
+
+user.left(2)
+user.forward(270)
+user.right(2)
+user.shapesize(8,8,1)
+
+user.left(2)
+user.forward(250)
+user.right(2)
+user.shapesize(9,9,1)
+
 wn.exitonclick()
+
+
+
