f42bb0d1304d80380855accff8b4e15bd3bf8f0f
Jeonghyeok Seo <jeonghyeok.seo@flemingcollege.ca>
2020-12-04 14:14:56 -0500

    update

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,54 +1,69 @@
-# Rock Paper Scissors Mini Game
-# Created By: IPGRP2 - J. Seo
-# December 2020
+#Jeonghyeok Seo
+#Rock Paper Scissors minigame
+#get user random selection Rock, Paper, or Scissors
+#get computer's random selection Rock, Paper, or Scissors
+#compare to computer choice
+#turtle moves forward if user wins
 
-#Greeting
-print("Welcome to Rock Paper Scissors Mini Game")
+      
+#Options of Rock, Paper and Scissors
 
-#user choose one value
+options = ["Rock", "Paper", "Scissors"]
 
-user = input("Please choose one form Rock, Paper, and Scissors :" )
+#Get user selection
+
+user = input("Please choose one form Rock, Paper, and Scissors :")
 user = user.capitalize()
-print(user)
+print("Player's Choice", user)
 
+#Error handling when user input other value
+while user.capitalize() not in options:
+   print("Incorrect input. Please enter Rock, Paper, or Scissors")
+   user = input("Please choose one form Rock, Paper, and Scissors :" )
+   user = user.capitalize()
+   print("Player's Choice", user)
 
 #Computer choose one value randomly
 
-
 import random
-Computer = random.choice(["Rock","Paper","Scissors"])
+Computer = random.choice(options)
 print(Computer)
 
 #Compare the values
 winner = None
+
+#When Computer select Rock
 if Computer == "Rock":
-    if user == "Scissors":
-       winner = "Computer"
-    elif user == "Paper":
-       winner = "user"   
-    else:
-       winner == None  
-
-elif Computer == "Paper":
-    if user == "Rock":
+   if user == "Scissors":
       winner = "Computer"
-    elif user == "Scissors":
-      winner = "user"
-    else:
-       winner == None  
+   elif user == "Paper":
+      winner = "user"   
+   else:
+      winner == None
 
+
+#When Computer select Paper
+elif Computer == "Paper":
+   if user == "Rock":
+      winner = "Computer"
+   elif user == "Scissors":
+      winner = "user"
+   else:
+      winner == None  
+
+#When Computer select Scissors
 else: 
-    if user == "Paper":
-       winner = "Computer"
-    elif user == "Rock":
-       winner = "user"
-    else:
-       winner == None
+   if user == "Paper":
+      winner = "Computer"
+   elif user == "Rock":
+      winner = "user"
+   else:
+      winner == None
 
 #Print the resaults
 if winner == "Computer" :
-    print("You lose")
+   print("You lose")
 elif winner == "user":
-    print("You win!")
+   print("You win!")
 else:
-    print("Ties")
+   print("Ties")
